9690f0092f0c07f833952fcf76cd0afa944f20f5
kryptocodes <srivatsantb@gmail.com>
2021-11-24 17:56:02 +0530

    Initial Commit
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import React from 'react'
+
+interface indexProps {
+
+}
+
+const index: React.FC<NextPage> = ({}) => {
+    return (
+        <div>
+            <h1>Hello Next.js</h1>
+        </div>
+    );
+}
+
+export default index
